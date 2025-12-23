8b69fcda8f0cc934663caab89f01dfcd66a5ec88
fardin-xec <abdullaxec123@gmail.com>
2025-12-23 17:20:14 +0530

    fix

--- src/services/autodesk-acc.service.ts
+++ src/services/autodesk-acc.service.ts
@@ -59,7 +59,7 @@ export class AutodeskAccService {
     return this.dataManagement.createFolder(projectId, parentFolderId, folderName);
   }
 
-   async createRotFolder(projectId: string, folderName: string) {
+   async createRootFolder(projectId: string, folderName: string) {
     return this.dataManagement.createRootFolder(projectId, folderName);
   }
 
